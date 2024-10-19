f1f33bd5799c7eee37014e62961ecb0e6c025809
parikshit-wayal <parikshitwayal3@gmail.com>
2024-10-19 21:43:18 +0530

    ok

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Signin from "./pages/Signin"
+import Signup from "./pages/Signup"
+import Dashboard from "./pages/Dashboard"
+import About from "./pages/about"
+import Home from "./pages/Home"
+import Projects from "./pages/Projects"
 
 export default function App() {
   return (
-    <div>App</div>
+      <BrowserRouter>
+      <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/Sign-in" element={<Signin/>} />
+          <Route path="/Sign-up" element={<Signup/>} />
+          <Route path="/Dashboard" element={<Dashboard />} />
+          <Route path="/Projects" element={<Projects />} />
+
+
+
+      </Routes>
+      
+      
+      </BrowserRouter>
+
   )
 }
